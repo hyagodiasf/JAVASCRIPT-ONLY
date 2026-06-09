43a710394267fc35d74255b14560924fab1b26ea
Hyago Diaz <hyagodiasf@gmail.com>
2026-06-09 13:18:51 -0300

    Novas funcionalidades

--- AI_IMAGE_GENERATOR/script.js
+++ AI_IMAGE_GENERATOR/script.js
@@ -1,4 +1,28 @@
 const themeToggle = document.querySelector('.theme-toggle');
+const promptForm = document.querySelector('.prompt-form');
+const promptInput = document.querySelector('.tprompt-input');
+const promptBtn = document.querySelector('.prompt-btn');
+const modelSelect = document.querySelector('model-select');
+const countSelect = document.querySelector('count-select');
+const ratioSelect = document.querySelector('ratio-select');
+
+const examplePrompts = [
+  "A magic forest with glowing plants and fairy homes among giant mushrooms",
+  "An old steampunk airship floating through golden clouds at sunset",
+  "A future Mars colony with glass domes and gardens against red mountains",
+  "A dragon sleeping on gold coins in a crystal cave",
+  "An underwater kingdom with merpeople and glowing coral buildings",
+  "A floating island with waterfalls pouring into clouds below",
+  "A witch's cottage in fall with magic herbs in the garden",
+  "A robot painting in a sunny studio with art supplies around it",
+  "A magical library with floating glowing books and spiral staircases",
+  "A Japanese shrine during cherry blossom season with lanterns and misty mountains",
+  "A cosmic beach with glowing sand and an aurora in the night sky",
+  "A medieval marketplace with colorful tents and street performers",
+  "A cyberpunk city with neon signs and flying cars at night",
+  "A peaceful bamboo forest with a hidden ancient temple",
+  "A giant turtle carrying a village on its back in the ocean",
+];
 
 (() => {
     const savedTheme = localStorage.getItem('theme');
@@ -15,4 +39,22 @@ const toggleTheme = () => {
     themeToggle.querySelector('i').className = isDarkTheme ? 'fa-solid fa-sun' : 'fa-solid fa-moon';
 }
 
+const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    const selectedModel = modelSelect.value;
+    const imageCount = parsetInt(countSelect.value) || 1;
+    const aspectRatio = ratioSelect.value || "1/1";
+    const promptText = promptInput.value.trim();
+
+    console.log(selectedModel, imageCount, aspectRatio, promptText);
+}
+
+promptBtn.addEventListener("click", () => {
+    const prompt = examplePrompts[Math.floor(Math.random() * examplePrompts.length)];
+    promptInput.value = prompt;
+    promptInput.focus();
+});
+
+promptForm.addEventListener("submit", handleFormSubmit);
 themeToggle.addEventListener('click', toggleTheme);
