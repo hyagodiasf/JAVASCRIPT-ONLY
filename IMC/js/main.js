edba1c11777617f722320f41b53566e83ab5d065
Hyago Diaz <104265180+hyagodiasf@users.noreply.github.com>
2026-08-30 10:47:55 -0300

    Finalizado o modelo de IMC básico
    Apenas treinando formas diferentes de aplicar o JavaScript

--- IMC/js/main.js
+++ IMC/js/main.js
@@ -2,29 +2,29 @@
 function limpar() {
     document.querySelector('.inpeso').value = '';
     document.querySelector('.inaltura').value = '';
-    document.querySelector('.resultadoP').value = '';
 }
+
 //FUNÇÃO CALCULAR
 function calcularImc(peso, altura) {
     return peso / altura ** 2;
 }
+
 //RESULTADO
 function resultado(msg) {
     const resul = document.querySelector('.resul');
     resul.innerHTML = msg;
 }
 
-
 const form = document.querySelector('.form');
 const imcP = document.querySelectorAll('.imc p');
+const resultadoP = document.querySelectorAll('.resultado p');
 
-
+//EVENTO DE SUBIMIT
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const inpeso = Number(e.target.querySelector('.inpeso').value);
     const inaltura = Number(e.target.querySelector('.inaltura').value);
-
 
     if (isNaN(inpeso) || inpeso <= 0) {
         resultado('Peso inválido!');
@@ -43,10 +43,11 @@ form.addEventListener('submit', (e) => {
     resultado(`Seu IMC é ${imcRes.toFixed(2)}`);
 
 
-    // Limpa as cores
+    // REMOVE CASO TENHA CORES
     for (let i = 0; i < imcP.length; i++) {
         imcP[i].classList.remove(
             'verde',
+            'verdeClaro',
             'amarelo',
             'laranja',
             'vermelho'
@@ -54,6 +55,7 @@ form.addEventListener('submit', (e) => {
 
         resultadoP[i].classList.remove(
             'verde',
+            'verdeClaro',
             'amarelo',
             'laranja',
             'vermelho'
@@ -64,24 +66,23 @@ form.addEventListener('submit', (e) => {
     let indice;
     let cor;
 
-
-    if (imcRes < 18.5) {
+    if (imcRes <= 18.5) {
         indice = 0;
         cor = 'verde';
 
-    } else if (imcRes >= 18.5 && imcRes <= 24.9) {
+    } else if (imcRes > 18.5 && imcRes <= 24.9) {
         indice = 1;
-        cor = 'verde';
+        cor = 'verdeClaro';
 
-    } else if (imcRes > 24.9 && imcRes <= 29,9) {
+    } else if (imcRes > 24.9 && imcRes <= 29.9) {
         indice = 2;
         cor = 'amarelo';
 
-    } else if (imcRes > 29,930 && imcRes <= 34,9) {
+    } else if (imcRes > 29.9 && imcRes <= 34.9) {
         indice = 3;
         cor = 'laranja';
 
-    } else if (imcRes > 34,9 && imcRes <= 39,9) {
+    } else if (imcRes > 34.9 && imcRes <= 39.9) {
         indice = 4;
         cor = 'vermelho';
 
