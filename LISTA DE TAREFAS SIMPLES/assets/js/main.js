f086190d5637184bc450df5a296038c2011eb24a
Hyago Diaz <hyagodiasf@gmail.com>
2026-09-11 10:51:44 -0300

    Updade main.js (LISTA DE TAREFAS)

--- LISTA DE TAREFAS SIMPLES/assets/js/main.js
+++ LISTA DE TAREFAS SIMPLES/assets/js/main.js
@@ -1,32 +1,42 @@
 const taskInput = document.querySelector('.taskInput');
 const addTaskBtn = document.querySelector('.addTaskBtn');
-const taskList = document.querySelector('.taskList');
 const removeTaskBtn = document.querySelector('.removeTaskBtn');
-let numberCounter = 0;
+const taskList = document.querySelector('.taskList');
 
 function createTaskItem(task) {
     const taskItem = document.createElement('li');
-    numberCounter++;
-    taskItem.textContent = `${numberCounter} - ${task}`;
+    taskItem.textContent = task;
     return taskItem;
 }
 
+function updateTaskNumbers() {
+    const tasks = taskList.querySelectorAll('li');
+    tasks.forEach((task, index) => {
+        task.textContent = `${index + 1} - ${task.textContent.replace(/^\d+ - /, '')}`;
+    });
+}
+
+function addTask() {
+    if (!taskInput.value) return;
+    taskList.appendChild(createTaskItem(taskInput.value.trim()));
+    updateTaskNumbers();
+    taskInput.value = '';
+}
+
 taskInput.addEventListener('keypress', (e) => {
-    if(e.key === 'Enter' && taskInput.value) {
-        taskList.appendChild(createTaskItem(taskInput.value));
-        taskInput.value = '';
+    if (e.key === 'Enter') {
+        addTask();
     }
 });
 
 addTaskBtn.addEventListener('click', (e) => {
-    if(!taskInput.value) return;
-    taskList.appendChild(createTaskItem(taskInput.value));
-    taskInput.value = '';
+    addTask();
 });
 
 removeTaskBtn.addEventListener('click', (e) => {
     const tasks = taskList.querySelectorAll('li');
-    if(tasks.length > 0) {
+    if (tasks.length > 0) {
         taskList.removeChild(tasks[tasks.length - 1]);
+        updateTaskNumbers();
     }
 });
