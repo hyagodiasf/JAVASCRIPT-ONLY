439f69f995f757cb8b088750efd44abb0c6feb1e
Hyago Diaz <hyagodiasf@gmail.com>
2026-09-23 12:52:54 -0300

    Update

--- CALCULADORA-FACTORY/asseets/js/main.js
+++ CALCULADORA-FACTORY/asseets/js/main.js
@@ -1,9 +1,13 @@
+//FUNÇÃO FACTORY É UMA FUNÇÃO QUE RETORNA 
+// UM NOVO OBJETO SEM NECESSIDADE DE USAR CLASSES OU OPERADOR
+
 function criaCalculadora() {
     return {
         display: document.querySelector('.display'),
         
         inicia() {
             this.cliqueBotoes();
+            this.pressionaEnter();
         },
         
         btnParaDisplay(valor) {
@@ -15,15 +19,34 @@ function criaCalculadora() {
         btnDelUm() {
             this.display.value = this.display.value.slice(0, -1);
         },
+        realizaConta() {
+            try {
+                this.display.value = eval(this.display.value);
+            } catch (error) {
+                this.display.value = 'Erro';
+            }
+        },
+        pressionaEnter() {
+            this.display.addEventListener('keypress', e => {
+                if (e.key === 'Enter') {
+                    this.realizaConta();
+                }
+            });
+        },
         cliqueBotoes() {
             document.addEventListener('click', e => {
                 const el = e.target;
+
                 if (el.classList.contains('btn-clear')) {
                     this.clearDisplay();
                     return;
                 }
                 if (el.classList.contains('btn-dell')) {
                     this.btnDelUm();
+                    return;
+                }
+                if(el.classList.contains('btn-eq')) {
+                    this.realizaConta();
                     return;
                 }
                 if (el.classList.contains('botao')) {
