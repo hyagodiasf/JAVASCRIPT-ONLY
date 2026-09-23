d926b7191cbc19ba3e26aa4f0a28286e4ee3a09d
Hyago Diaz <hyagodiasf@gmail.com>
2026-09-23 14:37:31 -0300

    CALCULADORA COM CONSTRUTOR
    Criando a mesma calculadora. Porém, usando outro forma em função, a construtora.

---
+++ CALCULADORA_CONSTRUTORA/asseets/js/main.js
@@ -0,0 +1,50 @@
+//é um molde usado para criar e inicializar novos objetos com propriedades e métodos repetidos.
+function Calculadora() {
+    this.display = document.querySelector('.display');
+
+    this.capturaCliques = () => {
+        document.addEventListener('click', event => {
+            const el = event.target;
+
+            if (el.classList.contains('btn-num')) {
+                this.addNumDisplay(el);
+            }
+            if (el.classList.contains('btn-clear')) {
+                this.clearDisplay();
+            }
+            if (el.classList.contains('btn-dell')) {
+                this.dellUmDisplay();
+            }
+            if (el.classList.contains('btn-eq')) {
+                this.eqCalculo();
+            }
+        });
+    };
+
+    this.inicia = () => {
+        this.capturaCliques();
+    };
+
+    this.addNumDisplay = el => {
+        this.display.value += el.innerText;
+    };
+
+    this.clearDisplay = () => {
+        this.display.value = '';
+    };
+
+    this.dellUmDisplay = () => {
+        this.display.value = this.display.value.slice(0, -1);
+    };
+
+    this.eqCalculo = () => {
+        try {
+            this.display.value = eval(this.display.value);
+        } catch (e) {
+            this.display.value = 'Erro';
+        }
+    }
+}
+
+const calculadora = new Calculadora();
+calculadora.inicia();
